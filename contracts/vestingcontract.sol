9cd38c14d4842771128443ebfc51c1cd1149cac7
thong2802 <ducthong2802@gmail.com>
2022-05-19 10:50:04 +0700

    add

--- contracts/vestingcontract.sol
+++ contracts/vestingcontract.sol
@@ -3,17 +3,20 @@ pragma solidity >=0.7.0 < 0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Tokenvesting is Ownable{
     using SafeMath for uint256;
     
     // info each user
-    struct UserInfo {
+    struct User {
         uint amount;
         uint tokenCanClaimed;
+        bool firstClaim;
+        uint256 firstJoin;
     }
     // mapping user info by address
-    mapping(address => UserInfo) public userInfo;
+    mapping(address => User) public userInfo;
     mapping(address => bool) whiteListUsers; // this is private
    
    // even 
